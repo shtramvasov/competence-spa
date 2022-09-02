14ca43589a3203aacca70646dc3654bbf240afc7
IrishAir <bls21@mail.ru>
2022-09-02 17:39:58 +0500

    hero section component added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,3 @@
-<!-- 
-
-  TODO:
-    1. добавление опциональных компонентов через слот? или придумать другое решение
-
- -->
-
-
 <template>
   <section>
     <Header />
@@ -14,10 +6,18 @@
       :title="'Работа с документами никогда не была на столько простой'"
       :desc="'Управляйте документами и адаптируйтесь к нуждам вашей команды. Просто. Из любого места.'"
       :centered="true"
-      
+      class="mb-340"
     >
     <CTAButtons class="jcc pb-120" :dark="true"/>
-    <img src="@/assets/img/hero-inventory.gif" class="rounded flex self-center">
+    <img src="@/assets/img/hero-inventory.gif" class="rounded flex self-center pb-64">
+    <div class="clients flex jcsb">
+      <img src="@/assets/img/client-salamander.svg" alt="Саламандр">
+      <img src="@/assets/img/client-askona.svg" alt="Аскона">
+      <img src="@/assets/img/client-gazprom.svg" alt="Газпрос">
+      <img src="@/assets/img/client-rosatom.svg" alt="Росатом">
+      <img src="@/assets/img/client-spar.svg" alt="Спар">
+      <img src="@/assets/img/client-mars.svg" alt="Марс">
+    </div>
     </HeroSection>
   </section>
 </template>
@@ -25,7 +25,7 @@
 <script>
 import Header from '@/components/Header/Header'
 import HeroSection from '@/components/HeroSection/HeroSection';
-import CTAButtons from '@/components/CTAButtons/CTAButtons.vue';
+import CTAButtons from '@/components/CTAButtons/CTAButtons';
 export default {
   name: 'HomeView',
   components: {
@@ -35,3 +35,15 @@ export default {
 }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '@/assets/sass/breakpoints';
+
+@media (max-width: $screen-medium) {
+  .clients {
+    flex-wrap: wrap;
+    gap: 3rem;
+    justify-content: center;
+  }
+}
+</style>
