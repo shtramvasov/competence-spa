0618c6e38d3034e75182123608ed4941d208a6bc
IrishAir <bls21@mail.ru>
2022-09-03 09:41:20 +0500

    Card component added

---
+++ src/components/Card/Card.vue
@@ -0,0 +1,62 @@
+<template >
+  <div class="container inner-wrapper flex gap-4" :class="{ 'dark grey__darken-4 rounded-lg' : this.dark == true }">
+    <div class="item item__text">
+      <h1 :class="{'grey__lighten-5--text' : this.dark == true}">{{ title }}</h1>
+      <slot />
+    </div>
+    <div class="item item__img flex cols jcc" :class="{ 'gap-3' : images.length > 1}">
+      <img v-for="image in images" :key="image" :src="require(`@/assets/img/${image.image}`)">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    dark: Boolean | false,
+    title: String,
+    images: Array,
+    cta: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/sass/breakpoints';
+  .container {
+    justify-content: center;
+
+    &.dark {
+      padding-top: 7.5rem;
+      padding-bottom: 7.5rem;
+    }
+  }
+
+  .item {
+    flex-wrap: wrap;
+
+    &__text {
+      max-width: 45%;
+
+      &.center {
+        max-width: 75%;
+        margin: 0 auto;
+      }
+    }
+  }
+
+  @media (max-width: $screen-medium) {
+
+    .container {
+      align-items: center;
+      flex-wrap: wrap;
+
+      .item {
+
+        &__text {
+          max-width: 100%;
+        }
+      }
+    }
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,6 +19,36 @@
       <img src="@/assets/img/client-mars.svg" alt="Марс">
     </div>
     </HeroSection>
+    <section>
+      <Card 
+        class="mb-300"
+        :title="'Документ для всех'"
+        :images="[{ image: 'the-doc-for-everyone.gif' }]"
+      >
+      <p class="light grey__darken-4--text">
+        Competence ощущается знакомо как .doc и интерактивно как приложение, поэтому вы можете сразу приступить к работе, 
+        начать эффективное и принимать решения.
+      </p>
+      </Card>
+      <Card 
+        class="mb-300"
+        :title="'В одном ритме с командой'"
+        :images="[{ image: 'fit-your-team.gif' }]"
+      >
+      <p class="light grey__darken-4--text">
+        Если команда растет или планы меняются, вы можете использовать «строительные блоки» для безшовной эволюции документа.
+      </p>
+      </Card>
+      <Card 
+        class="mb-300"
+        :title="'Доступ ко всему'"
+        :images="[{ image: 'all-access.gif' }]"
+      >
+      <p class="light grey__darken-4--text">
+        Автоматически синхронизируйте ваши дела в Google Calendar, обновления в Slack, Jira Issues и контролируйте их изменения.
+      </p>
+      </Card>
+    </section>
   </section>
 </template>
 
@@ -26,12 +56,14 @@
 import Header from '@/components/Header/Header'
 import HeroSection from '@/components/HeroSection/HeroSection';
 import CTAButtons from '@/components/CTAButtons/CTAButtons';
+import Card from '@/components/Card/Card';
 export default {
   name: 'HomeView',
   components: {
     Header,
     HeroSection,
-    CTAButtons
+    CTAButtons,
+    Card
 }
 }
 </script>
